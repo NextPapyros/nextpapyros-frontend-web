47fe027b84d3f3465fae25eb95729bb674d231c0
Demonqilin <velezloaizaesteban9012015@gmail.com>
2025-10-15 09:01:59 -0500

    build(config): add vite-plugin-vue-devtools for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import checker from "vite-plugin-checker";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), checker({ typescript: true })],
+  plugins: [vueDevTools(), vue(), tailwindcss(), checker({ typescript: true })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
